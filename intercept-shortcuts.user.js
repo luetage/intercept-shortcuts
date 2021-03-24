85aae921ea3cc4e4c604f09ea14b1f7afd72fd2c
luetage <luetage@vivaldi.net>
2021-03-24 10:27:16 +0100

    add ability to pass a shortcut

--- intercept-shortcuts.user.js
+++ intercept-shortcuts.user.js
@@ -2,16 +2,30 @@
 // @name       Intercept Shortcuts
 // @homepageURL https://github.com/luetage/intercept-shortcuts
 // @description Stops websites from hijacking keyboard shortcuts.
-// @version    0.7
+// @version    0.8
 // @match      *://*/*
 // @run-at     document-start
 
 {
     'use strict';
-    const keycodes = [9, 13, 16, 20, 27, 32, 37, 38, 39, 40]; //whitelists tab, enter, shift, capslock, escape, space and all arrow keys
+
+    let pass = () => {
+        si++;
+        if (si > 3) {
+            document.removeEventListener('keyup', pass);
+            si = 0;
+        }
+    }
+
+    let si = 0;
+    const keycodes = [9, 13, 16, 27, 32, 37, 38, 39, 40]; //whitelists tab, enter, shift, escape, space and arrow keys
     document.addEventListener('keydown', e => {
         // console.log(e.keyCode);
-        if (keycodes.indexOf(e.keyCode) === -1) {
+        if (e.keyCode === 74 && e.altKey) { //shortcut for letting the next shortcut pass (alt-j)
+            si = 1;
+            document.addEventListener('keyup', pass);
+        }
+        else if (si === 0 && keycodes.indexOf(e.keyCode) === -1) {
             e.cancelBubble = true;
             e.stopImmediatePropagation();
         }
