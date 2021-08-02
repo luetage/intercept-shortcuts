49dc30dcc02583901f86824eceba24765aebcdca
luetage <luetage@vivaldi.net>
2021-08-02 14:55:52 +0200

    make console.log optional

--- intercept-shortcuts.user.js
+++ intercept-shortcuts.user.js
@@ -20,7 +20,7 @@
     let si = 0;
     const keycodes = ['Escape', 'Tab', 'Enter', ' ', 'Shift', 'ArrowLeft', 'ArrowDown', 'ArrowUp', 'ArrowRight']; //whitelist
     document.addEventListener('keydown', e => {
-        console.log(e.key);
+        // console.log(e.key);
         if (e.key === 'j' && e.altKey) { //shortcut for letting the next shortcut pass (alt-j)
             si = 1;
             document.addEventListener('keyup', pass);
